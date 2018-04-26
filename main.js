6da8cafcfe3c0484578ebd98af6f78796715304c
it-spectre-ru <it.spectre.ru@gmail.com>
2018-04-26 18:55:59 +0500

    openModal func

---
+++ main.js
@@ -0,0 +1,9 @@
+let imageSF = document.querySelector('.trip-image');
+let backdrop = document.querySelector('.backdrop');
+let modal = document.querySelector('.modal');
+
+function openModal() {
+  backdrop.style.display = 'block';
+  modal.style.display = 'block';
+}
+
